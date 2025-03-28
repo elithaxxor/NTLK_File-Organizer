46355a8e14fca98f9ca66fa661da1fe0f0d55e77
adel <86808805+elithaxxor@users.noreply.github.com>
2025-03-27 23:00:13 -0400

    Add files via upload

---
+++ NLTK_File-Organizer/new2.sh
@@ -0,0 +1,13 @@
+pip3 install python-magic-bin
+pip3 install requests
+pip3 install beautifulsoup4
+pip3 install lxml
+pip3 install html5lib
+pip3 install cssutils
+pip3 install jsbeautifier
+pip3 install minify
+pip3 install autopep8
+pip3 install pylint
+pip3 install black
+
+
